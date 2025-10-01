fa92d58002f2ac7e979171e31d3abd3f8bbd02aa
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-10-01 10:14:02 -0500

    Refactor greet function to include greeting time

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function greet(name) {
-    console.log(`Welcome to Tech Evangelism Seminars, ${name}!`);
+    const hour = new Date().getHours();
+    const greetingTime = hour < 12 ? 'morning' : hour < 18 ? 'afternoon' : 'evening';
+    console.log(`Good ${greetingTime}, Welcome to Tech Evangelism Seminars, ${name}!`);
 }
 
 greet('Attendee');
