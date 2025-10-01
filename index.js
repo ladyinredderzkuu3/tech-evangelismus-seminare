dba2998cd9f3f6f032805ff44c7e4c5fea3ca7c3
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-10-01 10:14:18 -0500

    Refactor greeting message to include a friendly emoji.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function greet(name) {
     const hour = new Date().getHours();
     const greetingTime = hour < 12 ? 'morning' : hour < 18 ? 'afternoon' : 'evening';
-    console.log(`Good ${greetingTime}, Welcome to Tech Evangelism Seminars, ${name}!`);
+    console.log(`Good ${greetingTime} 😊, Welcome to Tech Evangelism Seminars, ${name}!`);
 }
 
 greet('Attendee');
